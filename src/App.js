c799267ed28b4c7dd9edd9dec986a32b81c5bd85
pusrover <pusrover@gmail.com>
2021-06-24 13:52:36 +0900

    #06-03

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import Movie from "./movie";
 
 class App extends React.Component {
 
@@ -14,7 +15,7 @@ class App extends React.Component {
             data: {
                 data: {movies},
             },
-        } = await axios.get('https://yts-proxy.now.sh/list_movies.json');
+        } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
         
         this.setState({ movies, isLoading: false});
     }
@@ -26,8 +27,17 @@ class App extends React.Component {
     }
 
     render(){
-        const {isLoading} = this.state;
-        return <div>{isLoading ? 'Loading.....' : 'We are ready'}</div>;
+        const {isLoading, movies} = this.state;
+        return <div>{isLoading ? 'Loading.....' : movies.map( (movie) => {
+            console.log(movie);
+            return (<Movie 
+                id = {movie.id}
+                year = {movie.year}
+                title = {movie.title}
+                summary = {movie.summary}
+                poster = {movie.medium_cover_image}
+            />);
+        })}</div>;
     }
 }
 export default App;
