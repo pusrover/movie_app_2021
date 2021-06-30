5036671f0112558fa91c09ae7143ea463f3d6f7f
pusrover <pusrover@gmail.com>
2021-06-30 11:58:19 +0900

    #08-03-01

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,6 +1,17 @@
 
+import './About.css';
+
 function About(){
-    return <span>About this page: I Built it because I love movies.</span>;
+    return (
+        <div className="about__container">
+            <span>
+                "Freedom is the freedom to say what two plus two make four. If that is granted, all else follows."
+            </span>
+            <span>
+                - George Orwell, 1984
+            </span>
+        </div>
+    );
 }
 
 export default About;
